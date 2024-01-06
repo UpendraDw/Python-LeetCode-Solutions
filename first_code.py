4739b06bc6eb0cd654a748e644a4a6e6d7d65f39
Upendra Dwivedi <elvn885@gmail.com>
2024-01-07 02:09:49 +0530

    deleted the python file because it was not required

--- first_code.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd 
-my_dict = {'FName':pd.Series(data = ['Upendra','Vivek','Abhishek']),'LName':pd.Series(['Dwivedi','Kumar','Sharma']),'Branch':pd.Series(['Electrical','Electrical'])}
-df = pd.DataFrame(data = my_dict)
-print(df)
